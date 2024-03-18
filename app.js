74474d1b971fea33a98072cd3420b34c9b6103c1
Manuel Salazar <mannisuso@gmail.com>
2024-03-18 10:44:34 -0500

    Create playGame function. According to the amount of round the user wants to play, the function loops X times, where in each iteration it gets userChoice, computerChoice and gets a winner.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// Array of options
+// Constants
 const OPTIONS = ['Rock', 'Paper', 'Scissors'];
+let userScore = 0;
+let computerScore = 0;
 
 // Get a random number
 function getRandomInt(max) {
@@ -15,28 +17,30 @@ function getComputerChoice () {
 // Computer input
 let computerChoice = getComputerChoice()
 
-// Get user input and capitalize it 
-let userInput = 'rock';
-let userChoice = userInput.charAt(0).toUpperCase() + userInput.slice(1);
-
 // Play round 
 function playRound(userChoice, computerChoice) {
     if (userChoice == 'Rock' && computerChoice == 'Scissors') {
+        userScore++;
         return('User wins! Rock beats scissors');
     }
     else if (userChoice == 'Paper' && computerChoice == 'Rock') {
+        userScore++;
         return('User wins! Paper beats rock');
     }
     else if (userChoice == 'Scissors' && computerChoice == 'Paper') {
+        userScore++;
         return('User wins! Scissors beat paper');
     }
     if (userChoice == 'Scissors' && computerChoice == 'Rock') {
+        computerScore++;
         return('Computer wins! Rock beats scissors');
     }
     else if (userChoice == 'Rock' && computerChoice == 'Paper') {
+        computerScore++;
         return('Computer wins! Paper beats rock');
     }
     else if (userChoice == 'Paper' && computerChoice == 'Scissors') {
+        computerScore++;
         return('Computer wins! Scissors beat paper');
     }
     else {
@@ -44,7 +48,18 @@ function playRound(userChoice, computerChoice) {
     }
 }
 
+// Play game
+function playGame(rounds) {
+    // Play the game X amount of times
+    for(let i = 0; i < rounds; i++) {
+        // Get user input and capitalize it
+        let userInput = prompt('Enter your choice: ');
+        let userChoice = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+        // Generate random computer choice
+        let computerChoice = getComputerChoice()    
+        // Print out the result of each round
+        console.log(playRound(userChoice, computerChoice)); 
+    }
+}
 
-console.log(userChoice);
-console.log(computerChoice);
-console.log(playRound(userChoice, computerChoice))
+console.log(playGame(5))
